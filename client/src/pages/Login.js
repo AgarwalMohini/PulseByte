779e6e5fefe1e95fd3279f395d4c58e254ca5823
Mohini Agarwal <160155298+AgarwalMohini@users.noreply.github.com>
2025-05-11 11:38:19 +0530

    added changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,33 @@
+import { Divider, Form, Input, Button} from 'antd'
+import { Link } from 'react-router-dom';
 import React from 'react'
 
 function Login() {
+
+  const onFinish=(values)=>{
+    console.log("Received values of Form:", values);
+
+  };
   return (
-    <div>Login</div>
+    <div className='authentication'>
+      <div className='authentication-form card p-3'>
+        <h1 className='card-title'>Welcome back</h1>
+        <Form layout='vertical' onFinish={onFinish}>
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='Email'/>
+          </Form.Item>
+
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Password' type='password'/>
+          </Form.Item>
+
+          <Button className='primary-button my-2' htmlType='submit' >LOGIN</Button>
+
+          <Link to='/register' className='anchor mt-3'> Click here to register</Link>
+        </Form>
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
